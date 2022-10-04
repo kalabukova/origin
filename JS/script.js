ea26fb37e3d1db1764d0a801e9acd6c1c0dc3f39
Nadya <example@gmaile.com>
2022-10-04 21:11:50 +0400

    first commit

---
+++ JS/script.js
@@ -0,0 +1,119 @@
+// "use strict";
+
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a:50
+};
+obj.a = 10;
+console.log (obj);
+console.log (name);
+var name = "Ivan";
+
+/* {
+    let result =50;
+}
+console.log (result); */
+
+/* const vehicleBodyWidth = 5000;
+const vehicleBodyLength = 4000;
+console.log("Ширина автомобиля: " + vehicleBodyWidth + ',длина: ' + vehicleBodyLength);  */
+/*  */
+/* ['vruru', 'ecvrev', 'veverguev'].map(a =>) */ 
+/* //Request запрос
+data
+response */
+
+//snake_case
+//UPPER_SNAKE_CASE
+//Kebab-case
+//PascalCase
+/* let COLOR_RED = '#0F00';
+const _apiBase
+/* const _apiKey */
+/* let number = 4.6;
+console.log(-4/0);
+console.log('string' * 9);
+const persone = "Alex";
+const bool = true; */ 
+/* console.log(something);null
+let und;
+console.log (und); */
+
+/* const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+
+}; */
+/* console.log(obj.name); */
+/* console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 6 , 'apple.bmp', {}, []];
+console.log(arr[1]); */
+// const arr = ['a', 'b', 'c'];
+// console.log(arr[1]);
+// const obj = {a: 1, b: 2};
+/* const obj = {
+    'Anna': 500,
+    'Alice': 800
+
+}; */
+/* const arr = ['a','b','c'];
+arr[10]= '3456';
+console.log(arr[10]);
+
+const arrObj {
+    0: 'a',
+    1: 'b',
+    2: 'c'
+    abc: {
+        df: [{}, {}],
+        def: {
+
+        }
+    }
+}
+const b='b';
+arrObj[b]= '1234';
+
+console.log(arrObj.b);
+console.log(arrObj['b']); */
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+/* const answer = +prompt("Вам есть 18", "18");
+console.log(answer + 5); */
+
+// const answers =[];
+/* answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваше фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', ''); */
+
+// console.log(typeof(answers));
+
+
+/* const category ='toys';
+console.log(`https:// someurl.com/${category}/5`); */
+
+/* const user = "Ivan";
+alert(`Привет, ${user}`); */
+
+
+/* console.log('arr' + "-object");
+console.log(4 + +"5"); */
+
+let incr = 10,
+    decr = 10;
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);
